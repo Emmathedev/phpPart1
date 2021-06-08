cae354b8537140063f408d72e6e4a58a9cf9a5e6
Emmathedev <emma.duflot@gmail.com>
2021-06-08 16:28:27 +0200

    push 1ere partie des exercices php

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ if($age >= 18){
 2. Micro-séisme impossible à ressentir mais enregistrable par les sismomètres
 3. Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti
 4. Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats
-5. Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de 		construction. Peu de dégats sur des bâtiments modernes
+5. Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes
 6. Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre
 7. Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance
 8. Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres
@@ -109,8 +109,7 @@ switch ($varMagn){
         echo '4. Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats';
         break;
     case 5:
-        echo '5. Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de 		construction. Peu de dégats sur des bâtiments modernes
-        ';
+        echo '5. Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes';
         break;
     case 6:
         echo '6. Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre';
